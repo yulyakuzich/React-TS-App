2205090afa5cb640aa02a450c296848e55594a6d
yulyakuzich <uljaonoff@gmail.com>
2023-12-03 19:34:03 +0300

    fix: delete msw

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,8 +1,1 @@
 import '@testing-library/jest-dom';
-import { afterAll, afterEach, beforeAll } from 'vitest';
-
-import { server } from './utils/msw/node';
-
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
